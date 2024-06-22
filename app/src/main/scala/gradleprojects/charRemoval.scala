4f7bb18ecaa183b11caf710238f83dc76d466dee
Jennifer-Dasari <jennifer.dasari@TECTORO.COM>
2024-06-22 23:41:23 +0530

    edited a line

--- app/src/main/scala/gradleprojects/charRemoval.scala
+++ app/src/main/scala/gradleprojects/charRemoval.scala
@@ -3,7 +3,7 @@ package gradleprojects
 //the given position will be in the range 0 .....string length -1inclusive
 object charRemoval {
   def main(args: Array[String]): Unit = {
-    println(removingChar("jennifer", 3))
+    println(removingChar("jr", 3))
   }
   def removingChar(a: String, b:Int): String = {
     a.take(b) + a.drop(b +1)
